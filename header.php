f56913f7e5c8b2df793d9c7a53c38ef1d66192e4
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-02-10 20:23:04 +0500

    Position

--- header.php
+++ header.php
@@ -360,7 +360,7 @@ $megaMenus = [
                     </a>
                 </div>
                 <!-- Navigation -->
-                <nav class="lg:w-full w-1/2 flex justify-center items-center">
+                <nav class="lg:w-full w-1/2 flex md:justify-center justify-end items-center">
                     <!-- MOBILE BUTTON -->
                     <button id="mobileMenuBtn" class="lg:hidden">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">

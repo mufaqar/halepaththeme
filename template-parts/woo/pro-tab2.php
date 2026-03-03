24da6ed31d2e2855f037c7adb7d55512776265e0
Mufaqar <mufaqar@gmail.com>
2026-03-03 13:19:10 +0500

    tabls

--- template-parts/woo/pro-tab2.php
+++ template-parts/woo/pro-tab2.php
@@ -1,11 +1,9 @@
-
-
 <section class="">
 
-<?php if ( have_rows('product_options', get_the_ID()) ) : ?>
-<?php while ( have_rows('product_options', get_the_ID()) ) : the_row(); ?>
+    <?php if ( have_rows('product_options', get_the_ID()) ) : ?>
+    <?php while ( have_rows('product_options', get_the_ID()) ) : the_row(); ?>
     <div class="hale_container flex md:flex-row flex-col gap-5 border-b border-gray-300 py-16">
-<?php
+        <?php
       // group field (sub field) inside the repeater row
       $group = get_sub_field('group_type'); // returns array of fields inside the group
       $type_id  = $group['type'] ?? null;      // taxonomy field
@@ -18,39 +16,39 @@
 
 
     ?>
-     <div class="md:w-1/5 w-full px-5">
-<h3 class="text-2xl font-bold text-title_Clr">
-    <?php     echo $term->name; ?>
-</h3>
-</div>
+        <div class="md:w-1/5 w-full px-5">
+            <h3 class="text-2xl font-bold text-title_Clr">
+                <?php     echo $term->name; ?>
+            </h3>
+        </div>
 
- <div class="md:w-4/5 w-full grid md:grid-cols-4 grid-cols-1 gap-5">
+        <div class="md:w-4/5 w-full grid md:grid-cols-4 grid-cols-1 gap-5">
 
-                <?php
+            <?php
                     if ($related_posts) :
                     foreach ($related_posts as $post) :
                         setup_postdata($post);
                         $code = get_field('option_code', $post->ID); 
                         ?>
 
-                        <div
+            <div
                 class="materialBox shadow-[0_0_2px_0_rgba(23,43,77,0.4)] hover:shadow-lg transition-all ease-in-out duration-300">
-                         
 
-                            <?php if (has_post_thumbnail()) : ?>
-                            <div class="w-full">
-                                <?php the_post_thumbnail('full'); ?>
-                            </div>
-                            <?php endif; ?>
-                            <div class="px-5 py-6 text-center">
+
+                <?php if (has_post_thumbnail()) : ?>
+                <div class="w-full">
+                    <?php the_post_thumbnail('full'); ?>
+                </div>
+                <?php endif; ?>
+                <div class="px-5 py-6 text-center">
                     <p class="text-base font-semibold text-title_Clr/80 mb-2">
                         <?php echo esc_html($code); ?>
                     </p>
                     <h3 class="text-lg leading-none font-semibold text-title_Clr">
-                          <h3><?php the_title(); ?></h3>
+                        <h3><?php the_title(); ?></h3>
                     </h3>
                 </div>
-                        </div>
+            </div>
 
             <?php
                                 endforeach;
@@ -58,19 +56,19 @@
                             endif; 
 
                     ?>
-         </div>
+        </div>
 
 
 
 
 
-            </div>
-<?php endwhile; ?>
-<?php endif; ?>
+    </div>
+    <?php endwhile; ?>
+    <?php endif; ?>
 
 
 
 
 
-   
+
 </section>

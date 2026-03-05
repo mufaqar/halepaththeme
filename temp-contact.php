05615c80bbe068e11b852f5408d9ac65be5215a8
Mufaqar <mufaqar@gmail.com>
2026-03-05 12:48:18 +0500

    contact

--- temp-contact.php
+++ temp-contact.php
@@ -1,39 +1,8 @@
 <?php
 /** Template Name: Contact */
-get_header();
-?>
-<?php
-    $steps = [
-        [
-            "id" => 1,
-            "title" => "Book Online",
-            "description" => "Lpsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been."
-        ],
-        [
-            "id" => 2,
-            "title" => "Choose Design",
-            "description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-        ],
-        [
-            "id" => 3,
-            "title" => "Confirmation",
-            "description" => "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-        ],
-        [
-            "id" => 4,
-            "title" => "Delivery",
-            "description" => "Lpsum is simply dummy text of the printing and typesetting is industry. Lorem Ipsum has been."
-        ],
-    ];
-    ?>
-
+get_header();?>
 <?php get_template_part('template-parts/get-qoute', ); ?>
-
 <?php get_template_part('template-parts/home-reviews'); ?>
-
-
 <?php get_template_part('template-parts/temp-instagram'); ?>
-
-
 <?php get_footer(); ?>
 
